b29a7289ca0ddeac73d5fd680f6b9af58b5b027d
Rohit Verma <rock.verma.rohit@gmail.com>
2020-04-14 21:17:16 +0530

    Add files via upload

---
+++ get.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
+from flask_cors import CORS
+from component import get_covid, country
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+class rest_api(Resource):
+    def get(self, country_name="world"):
+        Dict, countries = get_covid()
+
+        if country_name == "world":
+            return jsonify(Dict)
+        else:
+            country_name = str(country_name)
+            return jsonify(country(country_name, Dict, countries))
+
+
+api.add_resource(rest_api, "/world", endpoint="")
+api.add_resource(rest_api, "/<country_name>", endpoint="country")
+
+if __name__ == "__main__":
+    app.run()
